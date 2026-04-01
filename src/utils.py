12e2852b95cad9030b28a1e8e78cdfc86e78bc4a
jbernalg <jeinffersonbernal@gmail.com>
2026-04-01 10:21:01 -0500

    utils 1.3

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_auc_score, RocCurveDisplay, precision_recall_curve
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 import matplotlib.pyplot as plt
 
 def cv_score_interval(results, nombre: str, score: str = 'Recall'):
@@ -54,3 +55,48 @@ def plot_mat_confusion(model,
     plt.subplots_adjust(right=0.88)
     plt.tight_layout(pad=3.0)
     plt.show()
+
+
+def metricas_evaluacion(modelo, 
+                       x_test: pd.core.frame.DataFrame,
+                       y_test: pd.core.series.Series,
+                       nombre: str,
+                       conjunto: str,
+                       umbral: float=None):
+    '''
+    Calcula varias metricas de desempeno para un modelo de
+    clasificacion sobre un conjunto de datos de prueba
+    '''
+    
+    if umbral == None:
+        metricas = {}
+        y_pred = modelo.predict(x_test)
+        metricas['Model'] = nombre
+        metricas['Dataset'] = conjunto
+        metricas['Accuracy'] = np.round(accuracy_score(y_test, y_pred), 4)
+        metricas['Precision'] = np.round(precision_score(y_test, y_pred), 4)
+        metricas['Recall'] = np.round(recall_score(y_test, y_pred), 4)
+        metricas['F1-score'] = np.round(f1_score(y_test, y_pred), 4)
+        if hasattr(modelo, "predict_proba"):
+            y_proba = modelo.predict_proba(x_test)[:, 1]
+            metricas['AUC'] = np.round(roc_auc_score(y_test, y_proba), 4)
+        else:
+            metricas['AUC'] = '---'
+        metricas['Umbral'] = 0.50
+    else:
+        if hasattr(modelo, "predict_proba"):
+            metricas = {}
+            probs = modelo.predict_proba(x_test)[:, 1]
+            y_pred = (probs >= umbral).astype(int)
+            metricas['Model'] = nombre
+            metricas['Dataset'] = conjunto
+            metricas['Accuracy'] = np.round(accuracy_score(y_test, y_pred), 4)
+            metricas['Precision'] = np.round(precision_score(y_test, y_pred), 4)
+            metricas['Recall'] = np.round(recall_score(y_test, y_pred), 4)
+            metricas['F1-score'] = np.round(f1_score(y_test, y_pred), 4)
+            metricas['AUC'] = np.round(roc_auc_score(y_test, probs), 4)
+            metricas['Umbral'] = umbral
+        else:
+            raise ValueError('El modelo no cuenta con el método "predict_proba"')
+    
+    return metricas
